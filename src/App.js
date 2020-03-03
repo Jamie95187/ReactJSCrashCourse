de2292374d6dbba5c4255971d326ba574d1e6435
Jamie95187 <jamiewong63@googlemail.com>
2020-03-03 00:41:37 +0000

    feat: add new todoitem to list with uuid package

--- src/App.js
+++ src/App.js
@@ -2,23 +2,23 @@ import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
-import './components/App.css'
+import * as uuid from 'uuid';
 
 class App extends Component{
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with wife',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with dog',
         completed: false
       }
@@ -41,12 +41,22 @@ class App extends Component{
       !== id)] });
   }
 
+  // Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+
   render() {
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <AddTodo />
+          <AddTodo addTodo={this.addTodo}/>
           <Todos todos={this.state.todos} markComplete={this.markComplete}
           delTodo={this.delTodo}/>
         </div>
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
 class AddTodo extends Component {
+  state = {
+    title: ''
+  }
+
+  // e.target.value will get whatever we type into the text box
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.addTodo(this.state.title);
+    // Clear the field
+    this.setState({ title: ''})
+  }
+
   render() {
     return (
-      <form style={{display: 'flex'}}>
+      <form onSubmit={this.onSubmit} style={{display: 'flex'}}>
         <input type="text"
         name="title"
         style={{flex: '10', padding: '5px'}}
         placeholder="Add Todo ..."
+        value={this.state.title}
+        onChange={this.onChange}
         />
         <input
           type="submit"
